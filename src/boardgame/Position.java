733dd19c620d6d2353ce9fb3f9362ce8d17611cb
Rodrigo Burgos <rodrigoburgos2@gmail.com>
2021-02-12 17:04:10 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -13,12 +13,15 @@ public class Position {
 		return row;
 	}
 
+	public void setRow(int row) {
+		this.row = row;
+	}
+
 	public int getColumn() {
 		return column;
 	}
 
-	public void setValues(int row, int column) {
-		this.row = row;
+	public void setColumn(int column) {
 		this.column = column;
 	}
 
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private char col;
+	private int linha;
+
+	public ChessPosition(char col, int linha) {
+		if (col < 'a' || col > 'h' || linha < 1 || linha > 8) {
+			throw new ChessException("Erro: Informe valores de a1 a h8");
+		}
+		this.col = col;
+		this.linha = linha;
+	}
+
+	public char getCol() {
+		return col;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	protected Position toPosition() {
+		return new Position(8 - linha, col - 'a');
+	}
+
+	protected static ChessPosition fromPosition(Position pos) {
+		return new ChessPosition((char) ('a' - pos.getColumn()), 8 - pos.getRow());
+	}
+
+	@Override
+	public String toString() {
+		return "" + col + linha;
+	}
+
+}
