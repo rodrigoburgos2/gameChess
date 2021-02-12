d1bcee31b320646663289d4fb54207cd02bf2b03
Rodrigo Burgos <rodrigoburgos2@gmail.com>
2021-02-12 14:19:38 -0300

    Classes Piece e Board criadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -15,6 +16,7 @@ public class Program {
 		Position pos = new Position(3, 5);
 		System.out.println(pos);
 		
+		Board board = new Board(8, 8);
 		
 		
 		sc.close();
