a31c2a66f1ea5b9595da679815c847fe35a545e3
Rodrigo Burgos <rodrigoburgos2@gmail.com>
2021-02-12 13:58:58 -0300

    Classe Position criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 		
 		
